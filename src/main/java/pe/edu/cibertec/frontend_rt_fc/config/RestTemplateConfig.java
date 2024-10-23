2e0a388539a962b15eab31d5d1f73c009fcdecf9
Gad Josue Alva Castromonte <josuealva920@gmail.com>
2024-10-22 21:36:16 -0500

    updae url backend deployed

--- src/main/java/pe/edu/cibertec/frontend_rt_fc/config/RestTemplateConfig.java
+++ src/main/java/pe/edu/cibertec/frontend_rt_fc/config/RestTemplateConfig.java
@@ -10,7 +10,7 @@ public class RestTemplateConfig {
 
     @Bean
     public RestTemplate restTemplateAutenticacion(RestTemplateBuilder builder){
-        return builder.rootUri("backend-dawii-hycscharebf9gvdp.brazilsouth-01.azurewebsites.net/user")
+        return builder.rootUri("backend-dawii-hycscharebf9gvdp.mexicocentral-01.azurewebsites.net/user")
                 .build();
     }
 }
